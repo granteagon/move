8ff4c4a19f21c68f6e9b0cfea1369dfa77e2c549
granteagon <grant.eagon@gmail.com>
2016-06-13 10:52:35 -0400

    initial commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+export default function move (array, moveIndex, toIndex) {
+  /* #move - Moves an array item from one position in an array to another.
+
+     Note: This is a pure function so a new array will be returned, instead
+     of altering the array argument.
+
+    Arguments:
+    1. array     (String) : Array in which to move an item.         (required)
+    2. moveIndex (Object) : The index of the item to move.          (required)
+    3. toIndex   (Object) : The index to move item at moveIndex to. (required)
+  */
+  let itemRemovedArray = [
+    ...array.slice(0, moveIndex),
+    ...array.slice(moveIndex + 1, array.length)
+  ]
+  return [
+    ...itemRemovedArray.slice(0, toIndex),
+    array[moveIndex],
+    ...itemRemovedArray.slice(toIndex, itemRemovedArray.length)
+  ]
+}
